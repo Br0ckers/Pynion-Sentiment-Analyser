daade86765c8d12da8ddb8cdde1fc548548291cb
PreetiUdas <preetiudas@yahoo.com>
2018-12-17 17:43:23 +0000

    removed keys

--- config.py
+++ config.py
@@ -14,3 +14,9 @@ class Config(object):
     SECRET_KEY = "secret"
     DEBUG = True
     SQLALCHEMY_ECHO = True
+
+
+    # export API_KEY=E1xqzPEiqjlduYikpKjAkNS0w &&
+    # export API_SECRET_KEY=fJQVXAJNwR44wUUKrmi8Wf12O4wQ0YTmauEGSf3RITj5FzSVWh &&
+    # export ACCESS_TOKEN=1074639447310954496-jTBLObfNnTb1JZrGBUYvPRikxLIyrU &&
+    # export ACCESS_TOKEN_SECRET=2XZHahU34IwsL76KYX53nuKe3a74xoGIr9WgTIixf1qQF
